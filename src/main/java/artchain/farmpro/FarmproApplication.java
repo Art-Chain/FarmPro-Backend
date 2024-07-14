48ab363427a1608e6935e0e1340bb9c437a75605
woosung1223 <dntjd991223@naver.com>
2024-07-14 15:14:53 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/artchain/farmpro/FarmproApplication.java
@@ -0,0 +1,13 @@
+package artchain.farmpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artchain/farmpro/FarmproApplicationTests.java
@@ -0,0 +1,13 @@
+package artchain.farmpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
