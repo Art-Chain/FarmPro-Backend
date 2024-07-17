b56acad5670349c92c5b7c987118137a22a9fdeb
Hyoseop Song <crad_on25@naver.com>
2024-07-17 20:00:48 +0900

    :sparkles: feat: health check api 구현

---
+++ src/main/java/artchain/farmpro/global/health/HealthController.java
@@ -0,0 +1,12 @@
+package artchain.farmpro.global.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	@GetMapping("/health")
+ 	public String health() {
+ 		return "OK";
+ 	}
+}
