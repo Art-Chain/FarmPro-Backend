3c3e211fe1196aab49a9662513bf69a23fa9a98a
Hyoseop Song <crad_on25@naver.com>
2024-07-25 22:19:44 +0900

    :sparkles: feat: 마케팅 프로젝트 정보 참조를 통한 컨텐츠 생성 개선

--- src/main/java/artchain/farmpro/content/ContentRequest.java
+++ src/main/java/artchain/farmpro/content/ContentRequest.java
@@ -4,8 +4,13 @@ import artchain.farmpro.card.CardsRequest;
 import artchain.farmpro.crop.CropRequest;
 import java.util.List;
 
-public record ContentRequest(List<CropRequest> crops, ContentType contentType,
-                             ContentPurpose contentPurpose, CardsRequest cards, String mainText,
-                             ParlanceStyle parlanceStyle,
-                             String cardStyle) {
+public record ContentRequest(
+		ProjectInfoRequest projectInfo,
+		List<CropRequest> crops,
+		ContentType contentType,
+		ContentPurpose contentPurpose,
+		CardsRequest cards,
+		String mainText,
+		String parlanceStyle,
+		String cardStyle) {
 }
