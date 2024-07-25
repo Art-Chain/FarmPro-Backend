69791a11ed01fb1886513d928226de8575bc7207
Hyoseop Song <crad_on25@naver.com>
2024-07-25 22:10:36 +0900

    :sparkles: feat: 컨텐츠 목록 조회 API Presentation

--- src/main/java/artchain/farmpro/content/ContentController.java
+++ src/main/java/artchain/farmpro/content/ContentController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContentController {
 
 	private ContentService contentService;
-	
+
 	@Operation(summary = "컨텐츠 생성")
 	@PostMapping("/contents")
 	public ResponseEntity<List<ChatGptResponse>> createContent(@RequestBody ContentRequest request) {
@@ -37,5 +38,12 @@ public class ContentController {
 	public ResponseEntity<ContentResponses> getContentList() {
 		ContentResponses contentList = contentService.getContentList();
 		return ResponseEntity.ok(contentList);
+	}
+
+	@Operation(summary = "컨텐츠 상세 조회")
+	@GetMapping("/contents/{contentId}")
+	public ResponseEntity<ContentResponse> getContentDetail(@PathVariable Long contentId) {
+		ContentResponse contentDetail = contentService.getContentDetail(contentId);
+		return ResponseEntity.ok(contentDetail);
 	}
 }
